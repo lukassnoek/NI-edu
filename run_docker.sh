0317a673d1bc66738dd58063d29f5734053775e7
Lukas Snoek <lukassnoek@gmail.com>
2020-06-25 19:48:07 +0200

    Update dockerstuff

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,1 @@
-JUPYTER_TOKEN=test24
-docker run --rm -it -p 5901:5901 -p 9999:9999 -e JUPYTER_TOKEN=${JUPYTER_TOKEN} lukassnoek/niedu
+docker run -it -p 9999:9999 --name niedu_docker lukassnoek/niedu
